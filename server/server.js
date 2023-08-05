d9365915dbbab3d44b64f9d3fdc61b846af7cab8
imran_khan_941 <imran.khan@tkxel.io>
2023-08-05 19:00:51 +0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socket = require('socket.io');
+
+const app = express();
+const port = 3001;
+
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
+const server = http.createServer(app);
+const io = socket(server);
+
+
+
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (message) => {
+    console.log('Received message:', message);
+    socket.broadcast.emit('chat message', message);
+  })
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
